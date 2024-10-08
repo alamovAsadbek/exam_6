96d3dc5ad6c1e96ecb44c4e0bca6797e0bf8e27c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-09 02:27:46 +0500

    change

--- comments/admin.py
+++ comments/admin.py
@@ -1,19 +1,5 @@
 from django.contrib import admin
-from modeltranslation.admin import TranslationAdmin
 
 from comments.models import CommentModel
 
-
-@admin.register(CommentModel)
-class CommentAdmin(TranslationAdmin):
-    list_display = ('text', 'user', 'feedback')
-
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
+admin.site.register(CommentModel)
