955b6bc7452fa6b17abca72c4202110920c2bb13
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-08 20:17:04 +0500

    change

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -5,4 +5,9 @@ from .views import *
 urlpatterns = [
     path('auth/login', loginView, name='login'),
     path('auth/logout', logoutView, name='logout'),
+    path('auth/register', registerView, name='register'),
+    path('feedbacks', feedbacksView, name='feedbacks'),
+    path('feedbacks/create', offerFormView, name='create_feedback'),
+    path('comments/<int:feedback_id>', commentsView, name='create_comment')
+
 ]
