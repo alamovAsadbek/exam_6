ba77047864593cdd7ad65186295a02a505665a5e
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-08 20:25:10 +0500

    change

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -6,3 +6,11 @@ class FeedbackModel(models.Model):
     description = models.TextField()
     status = models.CharField(max_length=20)
     user = models.ForeignKey('UserModel', on_delete=models.CASCADE, null=True)
+    likes = models.IntegerField(default=0)
+    see = models.IntegerField(default=0)
+
+    choices = (
+        ('offer', 'Offer'),
+        ('problem', 'Problem')
+    )
+    feedback_type = models.CharField(max_length=20, choices=choices)
