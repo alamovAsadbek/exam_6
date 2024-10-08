f9c99e7a1736c436d244f904ff3d41cb91ea356b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-08 20:01:43 +0500

    change

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = (
+    ('en', 'English'),
+    ('uz', 'Uzbek'),
+)
 
 TIME_ZONE = 'UTC'
 
