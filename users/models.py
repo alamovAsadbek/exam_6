b1bca863214c1e759c171ea1c6f2f63ee7937bcd
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-08 19:53:20 +0500

    change

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class UserModel(models.Model):
+    full_name = models.CharField(max_length=100)
+    linkedin_url = models.URLField(max_length=200)
+    email = models.EmailField(max_length=100)
+    image = models.ImageField(upload_to='team_avatars/')
+    password = models.CharField(max_length=100)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.full_name
