2f46872099905b75c7d7ac1976e972bf0f89b96a
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-09 00:46:56 +0500

    change

--- config/urls.py
+++ config/urls.py
@@ -4,11 +4,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = []
 
 urlpatterns += i18n_patterns(
+    path('admin/', admin.site.urls),
     path('', include('feedbacks.urls'))
 )
 
