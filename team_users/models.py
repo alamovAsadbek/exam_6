5469c0c77b7b6b3661cd94789f9e63c2dcc033b4
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-08 22:48:45 +0500

    change

--- team_users/models.py
+++ team_users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class TeamUserModel(models.Model):
+    first_name = models.CharField(max_length=50, verbose_name=_('First Name'))
+    last_name = models.CharField(max_length=50, verbose_name=_('Last Name'))
+    image = models.ImageField(upload_to='team_users/images/', verbose_name=_('Image'))
+    role = models.CharField(max_length=100, verbose_name=_('Role'))
+    description = models.TextField(verbose_name=_('Description'))
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created At'))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated At'))
+
+    class Meta:
+        verbose_name = _('Team User')
+        verbose_name_plural = _('Team Users')
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
