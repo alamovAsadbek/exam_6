1586c04b16e2a28cd018479afa66f25f127c64b7
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-08 20:07:58 +0500

    change

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def feedbacksView(request):
     return render(request, 'offers/offer.html')
+
+
+# Home page view
+def landingPageView(request):
+    return render(request, 'index/index.html')
