825f6032be4167bf4eb8272fb49eabe9f3b46264
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-08 20:25:42 +0500

    change

--- feedbacks/models.py
+++ feedbacks/models.py
@@ -14,3 +14,13 @@ class FeedbackModel(models.Model):
         ('problem', 'Problem')
     )
     feedback_type = models.CharField(max_length=20, choices=choices)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Feedbacks'
+        verbose_name = 'Feedback'
