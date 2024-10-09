9bb8ad363920c651f5f887758ba5b3d7836490a9
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-09 16:30:36 +0500

    change

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 
 try:
