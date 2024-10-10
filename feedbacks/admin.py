ee2933ea6a23638a6d3b394c84148cc489a54b74
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-11 04:46:08 +0500

    change

--- feedbacks/admin.py
+++ feedbacks/admin.py
@@ -7,3 +7,13 @@ from .models import FeedbackModel
 @admin.register(FeedbackModel)
 class FeedbackModelAdmin(TranslationAdmin):
     list_display = ('id', 'title', 'description', 'status', 'created_at', 'updated_at')
+
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
