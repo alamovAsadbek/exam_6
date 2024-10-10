54545ea73980655676ccae84902632ae5b860746
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-11 03:09:56 +0500

    change admin.py

--- users/admin.py
+++ users/admin.py
@@ -6,5 +6,5 @@ from users.models import UserModel
 
 @admin.register(UserModel)
 class UserModelAdmin(TranslationAdmin):
-    list_display = ('id', 'full_name', 'email', 'status',)
-    list_display_links = ('id', 'full_name', 'email', 'status',)
+    list_display = ('id', 'full_name', 'email', 'is_active',)
+    list_display_links = ('id', 'full_name', 'email', 'is_active',)
