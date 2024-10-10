c8ab6f69e8416bd3e2c32aae9089527e50332e8e
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-10 18:39:58 +0500

    change

--- feedbacks/forms.py
+++ feedbacks/forms.py
@@ -5,15 +5,16 @@ from .models import FeedbackModel
 
 
 class FeedbackProblemForm(forms.ModelForm):
+    print("Problem form")
+
     class Meta:
         model = FeedbackModel
         fields = ['title', 'description', 'feedback_type']
 
 
 class FeedbackOfferForm(forms.ModelForm):
+    print("Offer form")
+
     class Meta:
         model = FeedbackModel
         fields = ['title', 'description', 'feedback_type', 'user']
-
-
-
