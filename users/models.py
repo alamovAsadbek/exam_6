c567cf28ddc223d8d2a87388bcec41f4bf737238
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-11 00:36:00 +0500

    change

--- users/models.py
+++ users/models.py
@@ -5,9 +5,9 @@ from django.utils.translation import gettext as _
 
 class UserModel(models.Model):
     full_name = models.CharField(max_length=100, verbose_name=_('Full Name'))
-    linkedin_url = models.URLField(max_length=200, verbose_name=_('LinkedIn URL'))
+    linkedin_url = models.URLField(max_length=200, verbose_name=_('LinkedIn URL'), null=True, blank=True)
     email = models.EmailField(max_length=100, verbose_name=_('Email'))
-    image = models.ImageField(upload_to='team_avatars/', verbose_name=_('Profile Image'))
+    image = models.ImageField(upload_to='team_avatars/', verbose_name=_('Profile Image'), null=True, blank=True)
     password = models.CharField(max_length=100, verbose_name=_('Password'))
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created At'))
