dc2834f5ed66e781baf0d2db5414da6e2bc7ad39
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-11 00:10:40 +0500

    change

--- users/views.py
+++ users/views.py
@@ -1,14 +1,14 @@
+from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate
-from django.core.exceptions import ValidationError
+from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMultiAlternatives
-from django.core.validators import validate_email
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
 from django.urls import reverse_lazy, reverse
 from django.utils.encoding import force_str, force_bytes
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
-from config.settings import EMAIL_HOST_USER
 from users.forms import UserRegisterForm, UserLoginForm
 from users.models import UserModel
 from users.token import email_token_generator
@@ -17,6 +17,44 @@ from users.token import email_token_generator
 def logoutView(request):
     logout(request)
     return render(request, 'index/index.html')
+
+
+def verify_email(request, uidb64, token):
+    uid = force_str(urlsafe_base64_decode(uidb64))
+    user = UserModel.objects.get(pk=uid)
+    if user is not None and email_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Account activated')
+        return redirect(reverse_lazy('users:login'))
+    else:
+        messages.error(request, 'The verification link is invalid.')
+        return redirect(reverse_lazy('users:login'))
+
+
+def send_email_verification(request, user):
+    token = email_token_generator.make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    current_site = get_current_site(request)
+    verification_url = reverse('users:verify_email', kwargs={'uidb64': uid, 'token': token})
+    full_url = f"http://{current_site.domain}/{verification_url}"
+
+    text_content = render_to_string(
+        'main/auth/register/verify-email.html',
+        {
+            'user': user,
+            'full_url': full_url,
+        }
+    )
+
+    message = EmailMultiAlternatives(
+        subject='Email Verification',
+        body=text_content,
+        to=[user.email],
+        from_email=settings.EMAIL_HOST_USER
+    )
+    message.attach_alternative(text_content, 'text/html')
+    message.send()
 
 
 def register_view(request):
@@ -28,94 +66,25 @@ def register_view(request):
             user.is_active = False
             user.save()
             send_email_verification(request, user)
-            return redirect(reverse_lazy('login'))
+            return redirect(reverse_lazy('users:login'))
         else:
             errors = form.errors
-            return render(request, 'auth/register/register.html', {'errors': errors})
-    else:
-        return render(request, 'auth/register/register.html')
-
-
-def profileView(request):
-    return render(request, 'profile/profile.html')
-
-
-def verify_email(request, uidb64, token):
-    try:
-        uid = force_str(urlsafe_base64_decode(uidb64))
-        user = UserModel.objects.get(pk=uid)
-        if email_token_generator.check_token(user, token):
-            user.is_active = True
-            user.save()
-            login(request, user)
-            return redirect(reverse_lazy('home'))
-        else:
-            return redirect(reverse_lazy('login'))
-    except Exception as e:
-        print(f'Error: {e}')
-        return redirect(reverse_lazy('login'))
-
-
-def send_email_verification(request, user):
-    token = email_token_generator.make_token(user)
-    uid = urlsafe_base64_encode(force_bytes(user.pk))
-    domain = request.get_host()
-    verification_url = reverse('verify_email', kwargs={'uidb64': uid, 'token': token})
-    full_url = f'http://{domain}{verification_url}'
-
-    text_content = render_to_string(
-        'components/verify_email/verify_email.html',
-        {'user': user, 'full_url': full_url}
-    )
-
-    message = EmailMultiAlternatives(
-        subject='Verify your email',
-        body=text_content,
-        from_email=EMAIL_HOST_USER,
-        to=[user.email]
-    )
-
-    message.attach_alternative(text_content, "text/html")
-    message.send()
+            return render(request, 'auth/register/register.html', {"errors": errors})
+    return render(request, template_name='auth/register/register.html')
 
 
 def login_view(request):
     if request.method == 'POST':
         form = UserLoginForm(request.POST)
         if form.is_valid():
-            username_or_email = form.cleaned_data['username_or_email']
+            username = form.cleaned_data['username']
             password = form.cleaned_data['password']
-
-            # Check if input is an email
-            try:
-                validate_email(username_or_email)
-                is_email = True
-            except ValidationError:
-                is_email = False
-
-            if is_email:
-                user_data = UserModel.objects.filter(email=username_or_email.lower()).first()
-                if user_data and user_data.is_active:
-                    username = user_data.username
-                else:
-                    username = None
+            user = authenticate(request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
             else:
-                user_data = UserModel.objects.filter(username=username_or_email.lower()).first()
-                if user_data and user_data.is_active:
-                    username = username_or_email
-                else:
-                    username = None
-
-            if username:
-                user = authenticate(request=request, username=username, password=password)
-                if user:
-                    login(request, user)
-                    return redirect(reverse_lazy('home'))
-                else:
-                    return render(request, 'auth/login/login.html', {
-                        'error': 'Invalid login details or account not activated. Please check your email.'
-                    })
-            else:
-                return render(request, 'auth/login/login.html', {'error': 'Invalid login details.'})
+                messages.error(request, 'Invalid username or password')
+                return render(request, template_name='auth/login/login.html')
     else:
         return render(request, 'auth/login/login.html')
