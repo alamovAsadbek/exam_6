620b6953646facf0a14906f475c9bc8cc30aa6a5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-10 13:43:08 +0500

    change

--- feedbacks/urls.py
+++ feedbacks/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('auth/register', registerView, name='register'),
     path('feedbacks', feedbacksView, name='feedbacks'),
     path('feedbacks/create', offerFormView, name='create_feedback'),
-    path('comments/<int:feedback_id>', commentsView, name='create_comment'),
+    path('comments/', commentsView, name='create_comment'),
     path('profile', profileView, name='profile')
 ]
