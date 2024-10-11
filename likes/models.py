80a384a99ce79c0668e89799ce24e1d154a57673
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-11 05:30:48 +0500

    change

--- likes/models.py
+++ likes/models.py
@@ -5,7 +5,14 @@ class LikeModel(models.Model):
     user = models.ForeignKey('users.UserModel', on_delete=models.CASCADE, null=True, blank=True, related_name='likes')
     post = models.ForeignKey('feedbacks.FeedbackModel', on_delete=models.CASCADE, null=True, blank=True,
                              related_name='post_likes')
+    status = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'Like'
         verbose_name_plural = 'Likes'
+
+    def __str__(self):
+        return f"{self.user} liked {self.post}"
