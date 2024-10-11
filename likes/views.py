d15d02d124050fcfa801862e5489819290af1239
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-11 12:15:35 +0500

    change

--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from likes.models import LikeModel
 
-# Create your views here.
+
+def like_post(request):
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        user_id = request.POST.get('user_id')  # Agar foydalanuvchi identifikatori kerak bo'lsa
+
+        # Yangi like qo'shish
+        LikeModel.objects.create(post=post_id, user=user_id)
