0f5292bd51146ac0a2d409373b71986af7cebe02
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-11 17:51:25 +0500

    change

--- team_users/admin.py
+++ team_users/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from modeltranslation.admin import TranslationAdmin
+from django.db import models
 
 from team_users.models import TeamUserModel
 
 
 @admin.register(TeamUserModel)
-class TeamUserAdmin(TranslationAdmin):
+class TeamUserAdmin(models.Model):
     list_display = ('id', 'first_name', 'last_name', 'role', 'description', 'created_at', 'updated_at')
