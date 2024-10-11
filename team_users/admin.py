ff5958f733a9b7603d8804d1b3f00ba1a1583e33
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-11 17:48:23 +0500

    change

--- team_users/admin.py
+++ team_users/admin.py
@@ -6,12 +6,4 @@ from team_users.models import TeamUserModel
 
 @admin.register(TeamUserModel)
 class TeamUserAdmin(TranslationAdmin):
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
+    list_display = ('id', 'first_name', 'last_name', 'role', 'description', 'created_at', 'updated_at')
