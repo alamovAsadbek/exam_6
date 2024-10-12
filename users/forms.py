2c3f19d99d56fcf8f850edffe5e409d1fd8146df
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-13 01:22:26 +0500

    change

--- users/forms.py
+++ users/forms.py
@@ -6,7 +6,7 @@ from users.models import UserModel
 class UserRegisterForm(forms.ModelForm):
     class Meta:
         model = UserModel
-        fields = ['full_name', 'email', 'password']
+        fields = ['full_name', 'username', 'password']
 
 
 class UserLoginForm(forms.Form):
