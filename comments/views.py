772d28971cde53a2144d4ce5840bcef3c9e4cbf5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-12 17:50:32 +0500

    change

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
 from comments.models import CommentModel
@@ -5,25 +6,29 @@ from feedbacks.models import FeedbackModel
 from .forms import CommentForm
 
 
+@login_required
 def createCommentView(request, pk):
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            form.cleaned_data['feedback'] = FeedbackModel.objects.get(pk=pk)
-            form.cleaned_data['user'] = request.user
+            comment = form.save(commit=False)
+            comment.user = request.user
             print(form.cleaned_data)
             form.save()
             return redirect('create_comment', pk=pk)
     elif request.method == 'GET':
-        feedback = FeedbackModel.objects.get(pk=pk)
-        feedback.see += 1
-        feedback.save()
-        comments = CommentModel.objects.filter(feedback=feedback)
-        print(comments)
-        context = {
-            'feedback': feedback,
-            'comments': comments
-        }
-        return render(request, 'comments/comment.html', context)
+        try:
+            feedback = FeedbackModel.objects.get(pk=pk)
+            feedback.see += 1
+            feedback.save()
+            comments = CommentModel.objects.filter(feedback=feedback)
+            print(comments)
+            context = {
+                'feedback': feedback,
+                'comments': comments
+            }
+            return render(request, 'comments/comment.html', context)
+        except FeedbackModel.DoesNotExist:
+            return render(request, '404/404.html')
     else:
         return render(request, '404/404.html')
