dba92b6bbf3db9447908d6f5b35e1086903dee67
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-12 22:19:18 +0500

    change

--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
+from django.conf import settings
 from django.contrib.auth import logout, login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMultiAlternatives
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.template.loader import render_to_string
+from django.urls import reverse_lazy, reverse
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserLoginForm
+from users.models import UserModel
+from users.token import email_token_generator
 
 
 def logoutView(request):
@@ -10,36 +18,74 @@ def logoutView(request):
     return render(request, 'index/index.html')
 
 
+def verify_email(request, uidb64, token):
+    uid = force_str(urlsafe_base64_decode(uidb64))
+    user = UserModel.objects.get(pk=uid)
+    if user is not None and email_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect(reverse_lazy('login'))
+    else:
+        return redirect(reverse_lazy('login'))
+
+
+def send_email_verification(request, user):
+    token = email_token_generator.make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    current_site = get_current_site(request)
+    verification_url = reverse('verify_email', kwargs={'uidb64': uid, 'token': token})
+    full_url = f"http://{current_site.domain}/{verification_url}"
+
+    text_content = render_to_string(
+        'components/verify_email/verify_email.html',
+        {
+            'user': user,
+            'full_url': full_url,
+        }
+    )
+
+    message = EmailMultiAlternatives(
+        subject='Email Verification',
+        body=text_content,
+        to=[user.email],
+        from_email=settings.EMAIL_HOST_USER
+    )
+    message.attach_alternative(text_content, 'text/html')
+    message.send()
+
+
 def register_view(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
+        print(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
+            user.is_active = False
             user.save()
-            return redirect(reverse_lazy('login'))
+            send_email_verification(request, user)
+            return redirect(reverse_lazy('users:login'))
         else:
             errors = form.errors
             return render(request, 'auth/register/register.html', {"errors": errors})
-    else:
-        form = UserRegisterForm()
-    return render(request, 'auth/register/register.html', {'form': form})
+    return render(request, template_name='auth/register/register.html')
 
 
 def login_view(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
-        user = authenticate(username=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('feedbacks')  # O'zingizga kerakli sahifaga yo'naltiring
-        else:
-            error_message = "Email yoki parol noto'g'ri"
-            return render(request, 'auth/login/login.html', {'error': error_message})
-
-    return render(request, 'auth/login/login.html')
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                print(request.user)
+                return redirect("feedbacks")
+            else:
+                return render(request, template_name='auth/login/login.html')
+    else:
+        return render(request, 'auth/login/login.html')
 
 
 def profileView(request):
