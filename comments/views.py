e789a4df6cb1a0734fb882c44f852889be70ab9b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-12 15:46:22 +0500

    change

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from comments.models import CommentModel
 from feedbacks.models import FeedbackModel
@@ -8,6 +8,11 @@ from .forms import CommentForm
 def createCommentView(request, pk):
     if request.method == 'POST':
         form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.feedback = FeedbackModel.objects.get(pk=pk)
+            comment.save()
+            return redirect('create_comment', pk=pk)
     elif request.method == 'GET':
         feedback = FeedbackModel.objects.get(pk=pk)
         feedback.see += 1
