c8bf8f48bcafaeeb91eb0a71969dbdd0a2d59a00
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-12 12:46:13 +0500

    cahnge

--- users/models.py
+++ users/models.py
@@ -1,13 +1,28 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import AbstractUser
-
 from django.contrib.auth.models import AbstractUser
-
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class UserModel(AbstractUser):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class UserModel(AbstractBaseUser):
     full_name = models.CharField(max_length=100, verbose_name=_('Full Name'))
     linkedin_url = models.URLField(max_length=200, verbose_name=_('LinkedIn URL'), null=True, blank=True)
     email = models.EmailField(max_length=100, verbose_name=_('Email'), unique=True)
@@ -20,24 +35,7 @@ class UserModel(AbstractUser):
     USERNAME_FIELD = 'email'  # Email ni asosiy identifikator sifatida belgilaymiz
     REQUIRED_FIELDS = ['full_name']  # Ro'yxatdan o'tishda talab qilinadigan maydonlar
 
-    groups = models.ManyToManyField(
-        Group,
-        related_name='custom_user_set',  # O'zgartiring
-        blank=True,
-        help_text=_('Guruhlar.'),
-        verbose_name=_('Guruhlar')
-    )
-
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='custom_user_permissions_set',  # O'zgartiring
-        blank=True,
-        help_text=_('Foydalanuvchilarga berilgan ruxsatlar.'),
-        verbose_name=_('Ruxsatlar')
-    )
-
-    def set_password(self, raw_password):
-        super().set_password(raw_password)  # Django'ning ichki mexanizmini ishlatish
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.full_name
@@ -45,8 +43,3 @@ class UserModel(AbstractUser):
     class Meta:
         verbose_name = _('User')
         verbose_name_plural = _('Users')
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._meta.get_field('last_login').remote_field.model._meta.local_fields.remove(
-            self._meta.get_field('last_login'))
