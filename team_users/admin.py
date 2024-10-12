2ffcf9c2fff9f7730190e55671aae4d723277d27
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-12 12:32:43 +0500

    change

--- team_users/admin.py
+++ team_users/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.db import models
 
 from .models import TeamUserModel
 
 
 @admin.register(TeamUserModel)
-class TeamUserAdmin(models.Model):
+class TeamUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name', 'last_name', 'role', 'description', 'created_at', 'updated_at')
