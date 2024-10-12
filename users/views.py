8b34692562992e6740e775812795f4f28d1a67d3
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-12 20:23:44 +0500

    change

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import logout, login
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
@@ -32,9 +33,9 @@ def login_view(request):
         form = UserLoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            user = UserModel.objects.filter(email=email).first()
-            print(user)
+            password = make_password(form.cleaned_data['password'])
+            user = UserModel.objects.filter(email=email, password=password).first()
+            print(user.password, user.email)
             if user is not None:
                 login(request, user)
                 return redirect('feedbacks')
