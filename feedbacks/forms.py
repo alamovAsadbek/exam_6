27ed88597a6b6e607f3e5f3843e254c9ae0d2dbe
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-14 00:23:38 +0500

    change

--- feedbacks/forms.py
+++ feedbacks/forms.py
@@ -4,13 +4,7 @@ from django import forms
 from .models import FeedbackModel
 
 
-class FeedbackProblemForm(forms.ModelForm):
-    class Meta:
-        model = FeedbackModel
-        fields = ['title', 'description', 'feedback_type']
-
-
-class FeedbackOfferForm(forms.ModelForm):
+class FeedbackRequestForm(forms.ModelForm):
     class Meta:
         model = FeedbackModel
         fields = ['title', 'description', 'feedback_type']
