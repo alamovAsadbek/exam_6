c3818a440ae87db1a58a6642623fb05c1b537038
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-13 09:48:04 +0500

    change

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.core.exceptions import ValidationError
 
 from users.models import UserModel
 
@@ -13,10 +12,3 @@ class UserRegisterForm(forms.ModelForm):
 class UserLoginForm(forms.Form):
     email = forms.EmailField(max_length=255, label='Email')
     password = forms.CharField(max_length=255, label='Password')
-
-
-# validation image for user registration form
-def validate_image_size(image):
-    max_size = 5 * 1024 * 1024
-    if image.size > max_size:
-        raise ValidationError("The maximum file size that can be uploaded is 5MB.")
