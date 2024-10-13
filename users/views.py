5d813f270baa925522cf702816ae851dbdb6905d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-13 16:30:30 +0500

    change

--- users/views.py
+++ users/views.py
@@ -80,7 +80,6 @@ def login_view(request):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user = UserModel.objects.filter(username=username, password=password).first()
-            print('user')
             print(user)
             if user is not None:
                 login(request, user)
