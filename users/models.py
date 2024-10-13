660cca8b7519997bf56923d369370b1423d1a4d5
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-13 11:13:46 +0500

    change

--- users/models.py
+++ users/models.py
@@ -28,7 +28,6 @@ class UserModelManager(BaseUserManager):
     def create_superuser(self, email, username, full_name, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_active', True)
-
         return self.create_user(email, username, full_name, password, **extra_fields)
 
 
@@ -38,17 +37,22 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=100, verbose_name=_('Email'), unique=True)
     is_active = models.BooleanField(default=False, verbose_name=_('Status'))
     username = models.CharField(max_length=100, verbose_name=_('Username'), unique=True)
-    image = models.ImageField(upload_to='team_avatars/', verbose_name=_('Profile Image'), null=True, blank=True,
-                              default='https://i.pinimg.com/736x/6a/d8/6f/6ad86fe68e2f55f29a0bf1a92d26a221.jpg',
-                              validators=[validate_image_size])
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',
+        blank=True
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',
+        blank=True
+    )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'full_name']
 
     objects = UserModelManager()
-
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created At'))
-    updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated At'))
 
     def __str__(self):
         return self.full_name
