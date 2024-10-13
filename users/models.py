37773970951d4453d632a1b6bd560d1e56557c1b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-13 09:38:21 +0500

    change

--- users/models.py
+++ users/models.py
@@ -1,27 +1,25 @@
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from .forms import validate_image_size
 
-class UserModel(AbstractBaseUser, PermissionsMixin):
+
+class UserModel(models.Model):
     full_name = models.CharField(max_length=100, verbose_name=_('Full Name'))
     linkedin_url = models.URLField(max_length=200, verbose_name=_('LinkedIn URL'), null=True, blank=True)
     email = models.EmailField(max_length=100, verbose_name=_('Email'), unique=True)
     is_active = models.BooleanField(default=False, verbose_name=_('Status'))
-    image = models.ImageField(upload_to='team_avatars/', verbose_name=_('Profile Image'), null=True, blank=True)
     username = models.CharField(max_length=100, verbose_name=_('Username'), unique=True)
+    image = models.ImageField(upload_to='team_avatars/', verbose_name=_('Profile Image'), null=True, blank=True,
+                              default='https://i.pinimg.com/736x/6a/d8/6f/6ad86fe68e2f55f29a0bf1a92d26a221.jpg',
+                              validators=[validate_image_size])
 
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('Created At'))
     updated_at = models.DateTimeField(auto_now=True, verbose_name=_('Updated At'))
-
-    USERNAME_FIELD = 'email'  # Use email as the unique identifier
-    REQUIRED_FIELDS = ['username', 'full_name']  # Required fields for createsuperuser
-
-    objects = BaseUserManager()
 
     def __str__(self):
         return self.full_name
@@ -29,21 +27,3 @@ class UserModel(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _('User')
         verbose_name_plural = _('Users')
-
-
-# User manager to handle user creation
-class UserModelManager(BaseUserManager):
-    def create_user(self, email, username, full_name, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, full_name=full_name, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, full_name, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_active', True)
-
-        return self.create_user(email, username, full_name, password, **extra_fields)
