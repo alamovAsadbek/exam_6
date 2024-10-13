fb547596d0bad504e2e0e73a45353285ee95ad8b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-14 01:32:35 +0500

    change

--- feedbacks/views.py
+++ feedbacks/views.py
@@ -13,7 +13,8 @@ def feedbacksView(request):
     offers = FeedbackModel.objects.all().filter(feedback_type='offer').exclude(user=request.user).order_by(
         '-created_at')
     my_offers = FeedbackModel.objects.all().filter(feedback_type='offer', user=request.user)
-    my_written_feedbacks = CommentModel.objects.all().filter(user=request.user).group_by('feedback')
+    my_written_feedbacks = CommentModel.objects.all().filter(user=request.user).order_by('-created_at')
+    print(my_written_feedbacks)
     context = {
         'problems': problems,
         'offers': offers,
